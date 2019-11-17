cd85b64fb8d60d544c9f9f64b230ed47ede89e4e
Kris Sinclair <kriss@trespass.co.uk>
2019-11-17 22:55:56 +0000

    Add progress helper

---
+++ helper/progress.js
@@ -0,0 +1,30 @@
+class Progress {
+    constructor(timeout) {
+        let now = new Date().getTime();
+        this.timeout = timeout;
+        this.nextResponse = now + timeout;
+    }
+
+    getProgress(position, length, message) {
+        let _return = {};
+
+        let now = new Date().getTime();
+        if (now >= this.nextResponse) {
+            this.nextResponse = now + this.timeout;
+
+            let percent = Math.round((position/length) * 100);
+            if (message) {
+                message = message.replace("%", percent + "%");
+                _return = message
+            } else {
+                _return = percent;
+            }
+        } else {
+            _return = false;
+        }
+
+        return _return;
+    }
+}
+
+module.exports = Progress;
