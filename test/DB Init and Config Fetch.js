37ee2880894543770cd7ce339a4454226dce58e6
Kris Sinclair <kriss@trespass.co.uk>
2020-02-13 13:44:24 +0000

    Add rotating logs feature

--- test/DB Init and Config Fetch.js
+++ test/DB Init and Config Fetch.js
@@ -2,7 +2,6 @@ const Chassis = require("../chassis");
 const DB = require("chassis-database");
 
 Chassis.bootstrap(function() {
-    console.log("Chassis Bootstrapped");
     DB.init(function (err) {
         if (err) {
 
